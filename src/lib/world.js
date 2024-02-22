8c2c4f25c1c8a1a2f61544209cc9652ad037b63e
Phil Zona <phil.zona@vercel.com>
2024-02-21 23:35:45 -0500

    Check for intersection with existing polygons when placing trees

--- src/lib/world.js
+++ src/lib/world.js
@@ -125,13 +125,29 @@ export class World {
     const top = Math.min(...points.map((p) => p.y));
     const bottom = Math.max(...points.map((p) => p.y));
 
+    // Areas trees should not be placed
+    const illegalPolys = [
+      ...this.buildings,
+      ...this.envelopes.map((e) => e.poly),
+    ];
+
     const trees = [];
     while (trees.length < count) {
       const p = new Point(
         lerp(left, right, Math.random()),
         lerp(bottom, top, Math.random()),
       );
-      trees.push(p);
+
+      let keep = true;
+      for (const poly of illegalPolys) {
+        if (poly.containsPoint(p)) {
+          keep = false;
+          break;
+        }
+      }
+      if (keep) {
+        trees.push(p);
+      }
     }
     return trees;
   }
