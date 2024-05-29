ef16f1fa499c8f035ac16cda01a175757fdd9416
Phil Zona <phil.zona@vercel.com>
2024-05-28 21:15:56 -0400

    Style parking space

--- src/markings/parking.js
+++ src/markings/parking.js
@@ -14,26 +14,27 @@ export class Parking extends Marking {
   constructor(center, directionVector, width, height) {
     super(center, directionVector, width, height);
 
-    this.border = this.poly.segments[2];
+    this.borders = [this.poly.segments[0], this.poly.segments[2]];
   }
 
   /**
    * @param {CanvasRenderingContext2D} ctx Context on which to draw
    */
   draw(ctx) {
-    this.border.draw(ctx, { width: 5, color: "white" });
+    for (const border of this.borders) {
+      border.draw(ctx, { width: 5, color: "white" });
+    }
 
     ctx.save();
     ctx.translate(this.center.x, this.center.y);
-    ctx.rotate(angle(this.directionVector) - Math.PI / 2);
-    ctx.scale(1, 2);
+    ctx.rotate(angle(this.directionVector));
 
     ctx.beginPath();
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
     ctx.fillStyle = "white";
-    ctx.font = `bold ${this.height * 0.3}px Arial`;
-    ctx.fillText("PARK", 0, 0);
+    ctx.font = `bold ${this.height * 0.7}px Arial`;
+    ctx.fillText("P", 0, 2.3);
     ctx.restore();
   }
 }
